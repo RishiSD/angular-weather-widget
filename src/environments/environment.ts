b1ad5773af7e7ec188a1ccb5019d49443dad317c
RishiSD <rishideorukhkar@gmail.com>
2022-03-12 23:19:33 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_ENDPOINT: ' https://api.openweathermap.org/data/2.5/weather?q=berlin&units=metric&appid=6cdf30431bb0b9d238240b66deddf5fb',
+  API_ENDPOINT: ' https://api.openweathermap.org/data/2.5/weather?q=berlin&units=metric&appid=<replace-with-actual-appid>',
   TitleOfSite: " Weather Card",
   City: " Berlin",
   WEATHER_ICON_URL: 'http://openweathermap.org/img/wn/'
